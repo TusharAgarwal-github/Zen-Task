ffba49f1c9cf01b402e48d77e61f40a9b946ba7f
Tushar agarwal <tusharagarwal@Tushars-MacBook-Air.local>
2020-11-11 23:25:29 +0530

    updating task4

--- task4/palindrome.js
+++ task4/palindrome.js
@@ -1,2 +1,23 @@
 const arr = ['carecar', 1344, 12321, 'did', 'cannot']
-console.log(arr[0])
+
+const palin = num => {
+	let a = [],
+		b = num.toString()
+	let i = 0,
+		j = b.length - 1
+	for (i; i < b.length; i++) {
+		a[i] = b[j--]
+	}
+
+	let c = a.join().replace(/,/g, '')
+
+	if (c === b) {
+		return true
+	} else {
+		return false
+	}
+}
+let data = arr.filter(item => {
+	return palin(item)
+})
+console.log(data)
--- task4/primeinarray.js
+++ task4/primeinarray.js
@@ -1,7 +1,9 @@
 const arr = [1, 3, 2, 5, 10]
 const PrimeArray = arr.filter(num => {
 	for (let i = 2; i < num; i++) {
-		if (num % i === 0) return false
+		if (num % i === 0) {
+			return false
+		}
 	}
 	return num !== 1
 })
